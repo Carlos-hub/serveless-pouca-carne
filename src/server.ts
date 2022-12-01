49f1301bb2389294262d69b04ed9c9d714e8fb75
Carlos-hub <delaracarlosmiguel@gmail.com>
2022-12-01 20:35:22 -0300

    alterando cors

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,18 @@
 import express, { json, NextFunction, Request, request, Response, response } from "express";
 import "express-async-errors";
+import cors from 'cors';
+
 import { routes } from "./routes";
+
+const allowedOrigins = ['http://localhost:3000', 'http://191.240.128.102'];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins
+};
+
 const app = express();
+
+app.use(cors(options))
 app.use(express.json())
 
 app.get("/",(req,res) =>{
