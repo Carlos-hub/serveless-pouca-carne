12570f623cdf9352ea616835f57387d3a616c1cc
Carlos-hub <delaracarlosmiguel@gmail.com>
2022-12-03 14:55:15 -0300

    alterando nome de colunas do banco

--- src/modules/cliente/useCases/createClient/createClientController.ts
+++ src/modules/cliente/useCases/createClient/createClientController.ts
@@ -4,14 +4,14 @@ import { CreateClientUseCase } from "./createClientUseCase";
 
 export class CreateClientController{
    async handle(request:Request,response:Response){
-    const {nome,email,cpf,telefone,dataNascimento,senha} = request.body;
+    const {nome,email,cpf,telefone,datanascimento,senha} = request.body;
     const createClientUseCase = new CreateClientUseCase();
     const result = await createClientUseCase.execute({
      nome,
      email,
      cpf,
      telefone,
-     dataNascimento,
+     datanascimento,
      senha
     })
     return response.status(201).json(result);
