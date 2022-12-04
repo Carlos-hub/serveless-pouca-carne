6bdc95d57fb3b1a0f157fc84a3dfe81d43d90e3d
Carlos-hub <delaracarlosmiguel@gmail.com>
2022-12-04 14:39:27 -0300

    alter acess control

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ app.use(cors(options))
 app.use(express.json())
 
 app.use((req,res,next) =>{
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173, https://pouca-carne.vercel.app/");
   res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
   app.use(cors());
 })
