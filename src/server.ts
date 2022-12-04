73b4614d38c9b759e566cc0995b04c93dcdb5f69
Carlos-hub <delaracarlosmiguel@gmail.com>
2022-12-04 16:51:28 -0300

    allow-orgiins

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import cors from 'cors';
 
 import { routes } from "./routes";
 
-const allowedOrigins = ['http://localhost:5173', 'http://191.240.128.102', 'pouca-carne-mkfddqw4y-carlos-hub.vercel.app', 'https://pouca-carne.vercel.app/'];
+const allowedOrigins = ['http://localhost:5173', 'http://191.240.128.102', 'https://pouca-carne.vercel.app/'];
 
 const options: cors.CorsOptions = {
   origin: allowedOrigins
